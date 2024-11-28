764658e300a3a30b0a4b40960ab5d09e7e67249c
Muhammadziyo Begaliyev <git@github.com:MuhammadziyoBegaliyev/AIOgram.git>
2024-11-28 18:20:05 +0500

    added bot

---
+++ practise_bot/lesson1_echobot.py
@@ -0,0 +1,20 @@
+# from aiogram import Bot , Dispatcher, types 
+# from asyncio import run 
+
+
+# dp = Dispatcher()
+#     await message.copy_to(chat_id=message.chat.id)
+
+
+
+# async def start():
+
+#     dp.message.register(echo)
+#     bot = Bot("7682918479:AAFIwfOI81ESlA1-KkZDyiueKX3_YrgL2Ik")
+#     await dp.start_polling(bot, polling_timeout=1)
+
+
+
+
+
+# run(start())
